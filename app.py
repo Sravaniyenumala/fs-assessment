ceaba5f4b9bc1e5b6f18b77484ac23c731d61dbf
sravaniyenumala <sravaniyenumala388@gmail.com>
2024-11-23 14:31:17 +0530

    added port

--- app.py
+++ app.py
@@ -168,6 +168,5 @@ def complete_assessment():
 
 init_db()
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
